b05b5be78317d1fc3523be331c443cf388fcbde5
Curtis Tan <merwinwee@gmail.com>
2020-10-24 20:41:49 +0800

    Use dynamic accumulate strategy

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "result_document" {
-  value       = data.aws_iam_policy_document.default.json
+  value       = length(var.source_documents) > 0 ? data.aws_iam_policy_document.next[length(var.source_documents) - 1].json : data.aws_iam_policy_document.empty.json
   description = "Aggregated IAM policy"
 }
